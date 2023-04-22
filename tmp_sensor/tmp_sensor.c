6f65c90731239ff7158e0d0d60e71fd11c43213a
ayswariya088 <ayka9480@colorado.edu>
2023-04-22 20:12:54 +0000

    logging to file all data

--- tmp_sensor/tmp_sensor.c
+++ tmp_sensor/tmp_sensor.c
@@ -4,6 +4,7 @@
 #include<sys/ioctl.h>
 #include<fcntl.h>
 #include<unistd.h>
+#include<syslog.h>
 /*
  * @function	: main.c for reading data through i2c bus
  *
@@ -11,26 +12,34 @@
  * @return		: RETURN  0 on success
  *
  */
+#define FILE_PATH "etc/tmpdir/tempdata.txt"
+
 int main()
 {
 int file_tmp;
+int file_fd; //to open the file for saving temp data
 unsigned char tmp_data[2]={0};
 char *i2c_bus ="/dev/i2c-1"; // bus Path in RPI we have configured for bus -1
 int temp_value;
 file_tmp=open(i2c_bus,O_RDWR);
 //open the file device
 if(file_tmp<0){
-printf("Error opening i2c_bus\n");
+syslog(LOG_ERR,"Error opening i2c_bus");
 exit(1);
 }
 
 //ioctl call for I2C
 if(ioctl(file_tmp,I2C_SLAVE, 0x48)<0){
-printf("Error while setting i2c slave device");
+syslog(LOG_ERR,"Error while setting i2c slave device");
 exit(1);
 }
 
 while(1){
+
+file_fd=open(FILE_PATH,O_WRONLY);
+if(file_fd<0){
+syslog(LOG_ERR,"Error opening tempdata.txt file");
+}
 //hold line low for starting communication
 write(file_tmp,0x00,1);
 
@@ -38,15 +47,21 @@ write(file_tmp,0x00,1);
 read(file_tmp,tmp_data,2);
 
 if(read <0){
+syslog(LOG_ERR,"error while reading data");
 printf("error while reading data\n");
-
+exit(1);
 }
 else{
 //tmp_sensor value is of 12 bits with  1st byte MSB and 2nd byte only the  d[7] to d[4] are part of the tmp reading
 temp_value=((tmp_data[0]<<4) |(tmp_data[1]>>4))*(0.0625); //converting to celcius
 
-printf("temperature value is %d C\n", temp_value);
+//printf("temperature value is %d C\n", temp_value);
+char temp_data[2];
+sprintf(temp_data,"%d",temp_value);
+write(file_fd,temp_data,sizeof(temp_data));
 usleep(1000000);
+
+close(file_fd);
 }
 
 }
