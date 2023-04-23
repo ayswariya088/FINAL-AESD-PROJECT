ec1eb0cec718f37ddfd23bee33bd96fe89a09c05
ayswariya088 <ayka9480@colorado.edu>
2023-04-23 11:06:16 -0600

    changes

--- tmp_sensor/tmp_sensor.c
+++ tmp_sensor/tmp_sensor.c
@@ -1,3 +1,12 @@
+/*
+ * @File name (temp_sensor.c)
+ * @File Description: (Application code for i2c based TMP 102 temperature sensor)
+ * @Author Name (AYSWARIYA KANNAN)
+ * @Date (04/23/2023)
+ * @Attributions : referred Project to understand
+ *                 how tmp 102 is interfaced https://github.com/cu-ecen-aeld/final-project-Amey2904dash/wiki/Project-Overview
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <linux/i2c-dev.h>
@@ -15,6 +24,9 @@
  *
  */
 #define FILE_PATH "/etc/tmpdir/tempdata.txt"
+#define SLAVE_ADDR (0x48)
+#define BUFFER_SIZE (50)
+#define LOW (0x00)
 
 int main()
 {
@@ -32,7 +44,7 @@ int main()
     }
 
     // ioctl call for I2C
-    if (ioctl(file_tmp, I2C_SLAVE, 0x48) < 0)
+    if (ioctl(file_tmp, I2C_SLAVE, SLAVE_ADDR) < 0)
     {
         syslog(LOG_ERR, "Error while setting i2c slave device");
         exit(1);
@@ -41,14 +53,14 @@ int main()
     while (1)
     {
 
-        file_fd = open(FILE_PATH, O_WRONLY,O_APPEND);
+        file_fd = open(FILE_PATH, O_WRONLY | O_APPEND); // in write only and append at the end of file
         if (file_fd < 0)
         {
             syslog(LOG_ERR, "Error opening tempdata.txt file");
             syslog(LOG_ERR, "%s", strerror(errno));
         }
         // hold line low for starting communication
-        write(file_tmp, 0x00, 1);
+        write(file_tmp, LOW, 1);
 
         // read after that from the slave
         read(file_tmp, tmp_data, 2);
@@ -65,10 +77,10 @@ int main()
             temp_value = ((tmp_data[0] << 4) | (tmp_data[1] >> 4)) * (0.0625); // converting to celcius
 
             // printf("temperature value is %d C\n", temp_value);
-            char send_buffer[50];
-            memset(send_buffer, 0, sizeof(send_buffer));
-            sprintf(send_buffer, "Temperature is %d\n", temp_value);
-            syslog(LOG_DEBUG, "Temperature is %d", temp_value);
+            char send_buffer[BUFFER_SIZE];
+            memset(&send_buffer[0], 0, sizeof(send_buffer));
+            sprintf(send_buffer, "Temperature is %d Deg C\n", temp_value);
+            syslog(LOG_DEBUG, "Temperature is %d Deg C ", temp_value);
             write(file_fd, send_buffer, sizeof(send_buffer));
             usleep(1000000);
         }
